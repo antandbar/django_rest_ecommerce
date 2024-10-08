634a49d2863fc78b1d2409a07dc15927acf8e7e3
Antonio Andreu <antonio.andreu.barreiro@gmail.com>
2024-10-08 12:04:08 +0200

    feat: Se implementa UpdateApiView para product

--- apps/products/api/urls.py
+++ apps/products/api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from apps.products.api.views.general_views import MeasureUnitListAPIView, IndicatorListAPIView, CategoryProductListAPIView
-from apps.products.api.views.product_views import ProductListAPIView, ProductCreateAPIView, ProductRetrieveAPIView, ProductDestroyAPIView
+from apps.products.api.views.product_views import ProductListAPIView, ProductCreateAPIView, ProductRetrieveAPIView, ProductDestroyAPIView, ProductUpdateAPIView
 
 urlpatterns = [
     path('measure_unit/', MeasureUnitListAPIView.as_view(), name = 'measure_unit'),
@@ -10,5 +10,6 @@ urlpatterns = [
     path('product/create/', ProductCreateAPIView.as_view(), name = 'product_create'),
     path('product/retrieve/<int:pk>', ProductRetrieveAPIView.as_view(), name = 'product_retrieve'),
     path('product/destroy/<int:pk>', ProductDestroyAPIView.as_view(), name = 'product_destroy'),
+    path('product/update/<int:pk>', ProductUpdateAPIView.as_view(), name = 'product_update'),
 
 ]
