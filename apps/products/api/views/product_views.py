0db74825572c10de21c5cd5944abc5aa217d7648
Antonio Andreu <antonio.andreu.barreiro@gmail.com>
2024-10-08 10:44:40 +0200

    feat: Se implementa CreateAPIView en product

--- apps/products/api/views/product_views.py
+++ apps/products/api/views/product_views.py
@@ -1,5 +1,20 @@
+from rest_framework import generics
+from rest_framework import status
+from rest_framework.response import Response
+
 from apps.base.api import GeneralListAPIView
 from apps.products.api.serializers.product_serializers import ProductSerializer
 
 class ProductListAPIView(GeneralListAPIView):
     serializer_class = ProductSerializer
+
+class ProductCreateAPIView(generics.CreateAPIView):
+    serializer_class = ProductSerializer
+
+    # No es necesio sobreescribir el método al heredar de CreateAPIView
+    def post(self, request):
+        serializer = self.serializer_class(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'message': ' Producto creado correctamente!'}, status = status.HTTP_201_CREATED)
+        return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
