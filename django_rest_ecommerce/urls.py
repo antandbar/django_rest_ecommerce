701d42d621afd26176316971ae0e423bd6f98dbb
Antonio Andreu <antonio.andreu.barreiro@gmail.com>
2024-10-10 08:50:23 +0200

    feat: Se implementa token y sesines

--- django_rest_ecommerce/urls.py
+++ django_rest_ecommerce/urls.py
@@ -6,6 +6,8 @@ from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
+from apps.users.views import Login
+
 schema_view = get_schema_view(
    openapi.Info(
       title="Documentación de PI",
@@ -24,7 +26,8 @@ urlpatterns = [
     path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls, name = 'login'),
+    path('',Login.as_view()),
     path('usuario/', include('apps.users.api.urls')),
     #path('products/', include('apps.products.api.urls')),
     path('products/', include('apps.products.api.routers')),
