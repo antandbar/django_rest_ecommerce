1238a1ee9bf7fb2ccee72c254cd4d3b3580fddda
Antonio Andreu <antonio.andreu.barreiro@gmail.com>
2024-10-12 13:21:37 +0200

    feat: Se implementa para CategoryProductViewSet las funciones retrieve, create y update

--- apps/products/api/views/general_views.py
+++ apps/products/api/views/general_views.py
@@ -1,4 +1,5 @@
 #from rest_framework import generics
+from rest_framework import status
 from rest_framework import viewsets
 from rest_framework.response import Response
 
@@ -60,6 +61,9 @@ class CategoryProductViewSet(viewsets.GenericViewSet):
     def get_queryset(self):
         return self.get_serializer().Meta.model.objects.filter(state = True)
     
+    def get_object(self):
+        return self.get_serializer().Meta.model.objects.filter(id=self.kwargs['pk'],state = True)
+    
     def list(self, request):
         """
         Retorna todas las unidades de medidas diponibles
@@ -71,6 +75,38 @@ class CategoryProductViewSet(viewsets.GenericViewSet):
         data = self.get_queryset()
         data = self.get_serializer(data, many = True)
         return Response(data.data)
+    
+    def retrieve(self, request, pk=None):
+        """
+        Retorna una categoría específica por su ID.
+        """
+        obj = self.get_object().get()
+        if obj:
+            serializer = self.serializer_class(obj)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response({'message': 'Categoría no encontrada'}, status=status.HTTP_404_NOT_FOUND)
+    
+    def create(self, request):
+        serrializer = self.serializer_class(data=request.data)
+        if serrializer.is_valid():
+            serrializer.save()
+            return Response({'message': 'Categoría registrada correctamente'}, status=status.HTTP_201_CREATED)
+        return Response({'message':'','error':serrializer.errors},status=status.HTTP_400_BAD_REQUEST)
+    
+    def update(self, request, pk=None):
+        if self.get_object().exists():    
+            serializer = self.serializer_class(isinstance=self.get_object().get(), data=request.data) 
+            if serializer.is_valid():
+                serializer.save()
+                return Response({'message': 'Categoría actualizada correctamente'}, status=status.HTTP_200_OK)
+        return Response({'message':'','error':serrializer.errors},status=status.HTTP_400_BAD_REQUEST)       
+
+    def destroy(self, request, pk=None):
+        if self.get_object.exist():
+            self.get_object().get().delete()
+            return Response({'message': 'Categoría actualizada correctamente'}, status=status.HTTP_200_OK)
+        return Response({'message':'','error':'Categoría no encontada'},status=status.HTTP_400_BAD_REQUEST)       
+
 
 '''
 class MeasureUnitListAPIView(GeneralListAPIView):
