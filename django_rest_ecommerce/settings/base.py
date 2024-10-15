a177b27446a2bfa7dbfa4988837d54c203134e56
Antonio Andreu <antonio.andreu.barreiro@gmail.com>
2024-10-15 12:28:39 +0200

    feat: Se implementa servir imagenes estáticas

--- django_rest_ecommerce/settings/base.py
+++ django_rest_ecommerce/settings/base.py
@@ -1,4 +1,5 @@
 from datetime import timedelta
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -144,6 +145,9 @@ SIMPLE_JWT = {
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [str(BASE_DIR / 'static')]
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- django_rest_ecommerce/urls.py
+++ django_rest_ecommerce/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.conf import settings
+from django.views.static import serve
 
 from rest_framework import permissions
 
@@ -41,4 +43,10 @@ urlpatterns = [
     path('users/', include('apps.users.api.routers')),
     #path('products/', include('apps.products.api.urls')),
     path('products/', include('apps.products.api.routers')),
+]
+
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {
+        'document_root': settings.MEDIA_ROOT
+    })
 ]
